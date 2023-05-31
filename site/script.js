7a016b81d92ae9d33bce6301bc044f6f72cde0f1
DGCK81LNN <triluolnn@163.com>
2023-05-31 21:11:49 +0800

    fix wrong uigf_version in exports

--- site/script.js
+++ site/script.js
@@ -922,7 +922,7 @@ function initialize() {
         export_timestamp: Math.floor(Date.now() / 1000),
         export_app: "dgck81lnn gi gacha visualize",
         export_app_version: "v0.1",
-        uigf_version: "v2.3",
+        uigf_version: "v2.2",
       },
       list: entryList.slice(0).reverse(),
     }
